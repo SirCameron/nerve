575d0225a2fb23cb96c6c95b0dbe9c3b45cecbd0
Cameron McAlpine <mcalpine@sircameron.com>
2021-04-27 23:06:31 +0200

    reorganised file structure

--- src/transmitters/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.ExampleTransmitter = require("./ExampleTransmitter");
-module.exports.BaseTransmitter = require("./BaseTransmitter");
-module.exports.RabbitMQTransmitter = require("./rabbitmq");
-module.exports.NerveTransmitter = require("./nerve");
